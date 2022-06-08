78d7494ece318ce9ba47be646112db7fb431d016
DiegucciX <98874031+DiegucciX@users.noreply.github.com>
2022-06-08 16:55:13 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.magneticForce(Dimension.Strength))
 })
-radio.setGroup(17)
+radio.setGroup(7567)
 basic.forever(function () {
     if (input.magneticForce(Dimension.Strength) < 100) {
         basic.showIcon(IconNames.SmallDiamond)
